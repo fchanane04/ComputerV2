de071636dd34bfddde6b6bce62b00a857c05ef3d
Fatima Chanane <93842426+fchanane04@users.noreply.github.com>
2025-06-19 01:11:15 +0000

    if it works dont touch it

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -5,9 +5,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from custom_types.mycomplex import MyComplex
 
-
-from custom_types.mycomplex import MyComplex
-
 a = MyComplex(3, 2)
 b = MyComplex(1, -4)
 
